5572901ca3239743b524a9b2f5e936924f0139ba
Lucas Azevedo <wormangel@gmail.com>
2015-09-28 22:21:31 -0300

    First layout change with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'user/dashboard'
 
-  get 'welcome/index'
+  get 'welcome/index', as: :dashboard
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
