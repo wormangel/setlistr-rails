433b632b7a01ac149650e5e36d0293ad519f4eb5
Lucas Azevedo <wormangel@gmail.com>
2015-10-07 21:51:23 -0300

    making sure creating a band initializes an empty setlist (unfortunately it was already implemented, but here are the specs)

---
+++ spec/models/band_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Band, type: :model do
+  it 'initializes an empty Setlist upon creation' do
+    expect { Band.create(attributes_for(:band)) }.to change(Setlist, :count).by(1)
+  end
+end
