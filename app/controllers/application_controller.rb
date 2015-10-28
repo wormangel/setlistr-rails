58d0d8b2d7b093fc9ce59ff81c3674019fd769f6
Lucas Azevedo <wormangel@gmail.com>
2015-10-28 20:23:44 -0300

    Exception handling on current_user (to avoid exception being thrown when user has cookie from previously authorized session and the db was reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,11 @@ class ApplicationController < ActionController::Base
   
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue Exception => e
+      nil
+    end
   end
   
   def profile_pic(size = 'square', uid = current_user.uid)
