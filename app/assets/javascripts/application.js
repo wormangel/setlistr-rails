c2e5678320936684efa9d89eb44c284115811d5f
Lucas Azevedo <wormangel@gmail.com>
2015-11-07 02:56:06 -0300

    Spinner display to address turbolinks behavior

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,4 +62,15 @@ $(document).on('page:load ready', function() {
       }
     }
   }
+  
+  startSpinner = function() {
+    $("html").css("cursor", "progress");
+  }
+  
+  stopSpinner = function() {
+    $("html").css("cursor", "auto");
+  }
+  
+  $(document).on("page:fetch", startSpinner);
+  $(document).on("page:receive", stopSpinner);
 });
